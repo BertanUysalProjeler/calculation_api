323f10463af9c1c0023563c81c58e69fa6ea9d93
BertanUysalProjeler <uysalbertan12@gmail.com>
2025-08-11 11:24:30 +0300

    Update thermal_conductor.py

--- calculation_api/calculations/thermal_conductor.py
+++ calculation_api/calculations/thermal_conductor.py
@@ -192,9 +192,10 @@ def compute_heat_load(
 
     return {
         "heat_load_watts": float(f"{heat_leak_watts:.3g}"),
-        "area_over_length_mm": a_over_l_mm,
+        "area_over_length_mm": float(f"{a_over_l_mm:.5g}"),
         "warnings": warnings
     }
+
 
 
 
